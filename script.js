fa569748ff8a4943d69830a6170ab059b36ce1f1
MatosoFelipe-dev <matoso.felipe@escola.pr.gov.br>
2026-09-02 01:01:29 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,88 @@
-import {perguntas} from './perguntas.js';
-
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
-const botaoJogarNovamente = document.querySelector(".novamente-btn");
-const botaoIniciar = document.querySelector(".iniciar-btn");
-const telaInicial = document.querySelector(".tela-inicial");
 
-let atual = 0;
+const perguntas = [
+    {
+        enunciado: "Você examina o quarto da viscondessa. Não há sinais de luta, ferimentos ou arrombamento. O corpo está deitado na cama, sem hematomas, mas com uma coloração rosada incomum na pele. A lareira estava acesa quando a encontraram.",
+        alternativas: [
+            {
+                texto: "Procurar por marcas de perfuração de agulhas nos braços.",
+                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer."
+            },
+            {
+                texto: "Procurar por cálices de vinho batizados no criado-mudo.",
+                afirmacao: "Quis saber como usar IA no seu dia a dia."
+            }           
+            
+        ]
+    },
+    {
+        enunciado: "Questionando o gabinete do castelo, você reúne os seis nomes presentes no palácio na noite do crime: Alden (o mordomo), Beatriz (a camareira), Caelen (o cocheiro), Daria (a cozinheira), Elian (o mensageiro atual) e Flora (a botânica real). Beatriz demonstra um nervosismo sutil ao mencionar a manutenção da lareira.",
+        alternativas: [
+            {
+                texto:"Interrogar Alden sobre a compra de venenos raros na farmácia.",
+                afirmacao:"Conseguiu utilizar a IA para buscar informações úteis."
+            },
+            {
+                texto: "Pressionar Flora sobre plantas tóxicas no jardim privado.",
+                afirmacao:"Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+            }
+        ]
+    },
+    {
+        enunciado: "Ao inspecionar a chaminé a fundo, você descobre que a saída de ar foi propositalmente vedada por dentro com trapos densos antes do fogo ser aceso, fazendo com que o gás invisível e inodoro se acumulasse no quarto lacrado.",
+        alternativas: [
+            {
+                texto:"Concluir que usaram um pó mágico de envenenamento pelo ar.",
+                afirmacao:"Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+            },
+            {
+                texto:"Buscar frascos de gás cianeto escondidos na cozinha de Daria.",
+                afirmacao:"Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+            }
+            
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativas: [
+            {
+                texto:"Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                afirmacao:"Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes"
+            },
+            {
+                texto:"Criar uma imagem utilizando um gerador de imagem de IA.",
+                afirmacao:"Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+            }
+            
+        ]
+    },
+    {
+        enunciado: " Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda de uma IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz?",
+        alternativas: [
+            {
+                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+                afirmacao:"Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+            },
+            {
+                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+                afirmacao:"Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+            }
+            
+            
+        ]
+    },
+];
+
+let atual = 0; 
 let perguntaAtual;
 let historiaFinal = "";
 
-botaoIniciar.addEventListener('click', iniciaJogo);
-
-function iniciaJogo() {
-    atual = 0;
-    historiaFinal = "";
-    telaInicial.style.display = 'none';
-    caixaPerguntas.classList.remove("mostrar");
-    caixaAlternativas.classList.remove("mostrar");
-    caixaResultado.classList.remove("mostrar");
-    mostraPergunta();
-}
-
 function mostraPergunta() {
-    if (atual >= perguntas.length) {
+    if(atual >= perguntas.length){
         mostraResultado();
         return;
     }
@@ -36,8 +92,8 @@ function mostraPergunta() {
     mostraAlternativas();
 }
 
-function mostraAlternativas() {
-    for (const alternativa of perguntaAtual.alternativas) {
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
         botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
@@ -45,29 +101,17 @@ function mostraAlternativas() {
     }
 }
 
-function respostaSelecionada(opcaoSelecionada) {
-    const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacao;
     historiaFinal += afirmacoes + " ";
-    if(opcaoSelecionada.proxima !== undefined){
-        atual = opcaoSelecionada.proxima;
-    }else{
-        mostraResultado();
-        return;
-    }
+    atual++;
     mostraPergunta();
 }
 
-function mostraResultado() {
-    caixaPerguntas.textContent = `Com Sucesso, RLM`;
+function mostraResultado(){
+    caixaPerguntas.textContent = "Em 2049...";
     textoResultado.textContent = historiaFinal;
-    caixaAlternativas.textContent = "";
-    caixaResultado.classList.add("mostrar");
-    botaoJogarNovamente.addEventListener("click", jogaNovamente);
+    caixaAlternativas.textContent = ""; 
 }
 
-function jogaNovamente(){
-    atual = 0;
-    historiaFinal = "";
-    caixaResultado.classList.remove("mostrar");
-    mostraPergunta();
-}
+mostraPergunta();
