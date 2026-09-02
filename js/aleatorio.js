e96d4e124b3416340c236f7bf284f59cbfa50161
MatosoFelipe-dev <matoso.felipe@escola.pr.gov.br>
2026-09-02 00:40:00 -0300

    Delete js/aleatorio.js

--- js/aleatorio.js
+++
@@ -1,8 +0,0 @@
-const nomes = ["RLM", "FPH", "ANG", "PRT", "JJT","UYV", "KST"];
-
-export function aleatorio (lista){
-    const posicao = Math.floor(Math.random()* lista.length);
-    return lista[posicao];
-}
-
-export const nome = aleatorio(nomes);
